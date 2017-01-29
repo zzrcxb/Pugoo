91c70c13a33285b3220426dcd7583c1f9ab17f4c
zRui <zzrcxb@users.noreply.github.com>
2017-01-29 19:50:17 +0800

    Delete run.py

--- run.py
+++
@@ -1,5 +0,0 @@
-from DataManagement.add2set import add2set
-from HashSys.hashsys import clear_dirs
-
-# clear_dirs()
-add2set(r'D:\Users\Neil Zhao\PyCharm\Pugo\Database\raw\nngs', source='test', log_path=r'.\Log')
